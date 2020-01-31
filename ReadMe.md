ebad48054d6b0c3bc9da97a77080b29e449189da
Phanish <phanish.m@target.com>
2020-01-31 15:00:09 -0600

    Initial commit

---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+docker build -t demo_spring .
+docker run -p 8080:8080 -t demo_spring
+http://localhost:8080/demo
---
+++ src/main/java/com/example/demo/controllers/DemoController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    @RequestMapping(path = "/demo", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity demo() {
+        return new ResponseEntity<>("Success", HttpStatus.OK);
+    }
+}
